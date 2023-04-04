7d6781a6870eec0cf8f7a6dfed106018761a1247
Karuna-Gupta <129582036+Karuna-Gupta@users.noreply.github.com>
2023-04-04 17:40:45 +0530

    first cut

---
+++ ConcurrentCount/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter path for files:");
+string path = Console.ReadLine();
+string[] files;
+if (path != null)
+{
+    string filter = "*.txt";
+    files = Directory.GetFiles(path, filter);
+
+
+    Task.Factory.StartNew(() =>
+    {
+        for (int i = 0; i < files.Length; ++i)
+        {
+
+            CountWords(files[i]);
+            if (i % 5 == 0)
+            {
+                Thread.Sleep(2000);
+            }
+        }
+    });
+}
+
+void CountWords(string path)
+{
+    string[] content = File.ReadAllLines(path);
+    foreach (string line in content)
+    {
+        Console.WriteLine(line);
+    }
+    
+}
+
+Console.ReadLine();
+
+
